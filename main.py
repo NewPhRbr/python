d718523ccc473d58eedbe7419a8105e6b5375eed
NewPhRbr <106818085+NewPhRbr@users.noreply.github.com>
2024-05-06 07:36:10 +0000

    попытка 2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import panel as pn
 from model import Person
+from db import SessionLocal
 
 p = Person()
 p.region = "Moscow"
+
 
 button = pn.widgets.Button(name="Click me", icon="caret-right", button_type="primary")
 button_output = pn.widgets.Button(
@@ -13,14 +15,14 @@ input = pn.widgets.TextInput(name="Write me", value=p.region)
 
 components = pn.Column(pn.Row(button, input), pn.Row(button_output, output))
 
-
-def handle_click(event):
-    p.region = input.value
-
-
-def output_click(event):
-    output.value = p.region
-
+with SessionLocal as session:
+    def handle_click(event):
+        p.region = input.value
+        session.add(p)
+        session.commit()
+    
+    def output_click(event):
+        output.value = p.region
 
 button_output.on_click(output_click)
 button.on_click(handle_click)
